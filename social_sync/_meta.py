1d91f8210cbccd6167789f14f107866a7bed42f5
Carlos Larriu <larriucarlos@gmail.com>
2023-05-02 22:18:00 +0200

    modified meta to fix lint

--- social_sync/_meta.py
+++ social_sync/_meta.py
@@ -3,7 +3,14 @@
 """social_sync package metadata"""
 
 __title__ = "social_sync"
-__description__ = "Social Sync is a software tool that helps content creators automate the process of uploading and sharing videos across multiple social media platforms, including TikTok, YouTube, Twitter, and Instagram. With Social Sync, users can easily manage and schedule their posts, allowing them to focus on creating great content without worrying about the logistics of social media. This tool helps to increase social media presence and grow followers by making sharing videos easy and efficient."
+__description__ = ("Social Sync is a software tool that helps content creators "
+                   "automate the process of uploading and sharing videos across "
+                   "multiple social media platforms, including TikTok, YouTube, "
+                   "Twitter, and Instagram. With Social Sync, users can easily manage "
+                   "and schedule their posts, allowing them to focus on creating "
+                   "great content without worrying about the logistics of social media. "
+                   "This tool helps to increase social media presence and grow followers "
+                   "by making sharing videos easy and efficient.")
 __url__ = "https://github.com/clarriu97/social-sync"
 __version__ = "0.1.0"
 __author__ = "clarriu97"
